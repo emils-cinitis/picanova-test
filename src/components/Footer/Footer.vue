d1679586532f0601d615d1b4fff13978861c8074
Emils Cinitis <emis_2@inbox.lv>
2022-05-19 17:20:32 +0300

    Fix footer class name

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -9,7 +9,7 @@
     import { Component, Vue } from "vue-property-decorator";
 
     @Component
-    export default class Home extends Vue {}
+    export default class Footer extends Vue {}
 </script>
 
 <style lang="scss" scoped>
